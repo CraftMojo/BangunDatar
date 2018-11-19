05299dfafc55aca97875facc1283829b56a32226
CrafMojo <setiawan.yanto@gmail.com>
2018-11-19 12:30:44 +0700

    add comment untuk test

--- src/com/craftmojo/Lingkaran.java
+++ src/com/craftmojo/Lingkaran.java
@@ -14,6 +14,6 @@ public class Lingkaran implements Shape {
         System.out.print("Masukkan jari-jari: ");
         r = scan.nextDouble();
 
-        //test all cases
+        //test all cases test
     }
 }
