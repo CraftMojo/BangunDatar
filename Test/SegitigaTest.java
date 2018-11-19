e71207618cf7b19c68f0bf5bcacf079257545330
CrafMojo <setiawan.yanto@gmail.com>
2018-11-19 12:18:57 +0700

    update test

--- Test/SegitigaTest.java
+++ Test/SegitigaTest.java
@@ -6,10 +6,10 @@ public class SegitigaTest {
     @Test
     public void hitungLuas() {
         Segitiga s1 = new Segitiga();
-        s1.setTinggi(8);
+        s1.setTinggi(5);
         s1.setAlas(10);
         double expected, actual;
-        expected = 40.0;
+        expected = 25.0;
         actual = s1.hitungLuas();
 
         Assert.assertEquals(expected, actual,0.0f);
